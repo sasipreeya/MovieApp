330555399b097327455eb9ee2e1b223c6541656e
Sasipreeya Sukprasert <sasipreeya.sukprasert@scb.co.th>
2020-04-02 14:39:44 +0700

    Installed redux and did the search button function

--- src/components/SearchComponent.js
+++ src/components/SearchComponent.js
@@ -1,16 +1,32 @@
 import React from "react";
 import SearchBar from "react-native-search-bar";
 
+export var searchHistory = [];
+
 export default class SearchComponent extends React.Component {
+  state = {
+    search: ""
+  };
+
+  updateSearch = search => {
+    this.setState({ search });
+  };
+
+  onSearchClicked = searchWords => {
+    searchHistory.push(searchWords);
+    console.log(searchHistory);
+  };
+
   render() {
     return (
-        <SearchBar
-            ref="searchBar"
-            placeholder="Search"
-            // onChangeText={...}
-            // onSearchButtonPress={...}
-            // onCancelButtonPress={...}
-        />
+      <SearchBar
+        ref="searchBar"
+        placeholder="Search"
+        onChangeText={this.updateSearch}
+        onSearchButtonPress={() => {
+          this.onSearchClicked(this.state.search);
+        }}
+      />
     );
   }
 }
