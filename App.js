8ba4b95d798c30dc47705cb68eaf26f27e7d37cc
Sasipreeya Sukprasert <sasipreeya.sukprasert@scb.co.th>
2020-04-03 15:38:35 +0700

    Removed unused code

--- App.js
+++ App.js
@@ -8,14 +8,6 @@ import {
   StatusBar
 } from "react-native";
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions
-} from "react-native/Libraries/NewAppScreen";
-
 import { connect } from "react-redux";
 import { ListItem } from "react-native-elements";
 import NavBar from "./src/components/NavbarComponent";
@@ -43,44 +35,5 @@ function mapStateToProps(state) {
     searchHistory: state.searchReducer.searchHistory
   };
 }
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter
-  },
-  engine: {
-    position: "absolute",
-    right: 0
-  },
-  body: {
-    backgroundColor: Colors.white
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: "600",
-    color: Colors.black
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: "400",
-    color: Colors.dark
-  },
-  highlight: {
-    fontWeight: "700"
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: "600",
-    padding: 4,
-    paddingRight: 12,
-    textAlign: "right"
-  }
-});
 
 export default connect(mapStateToProps)(App);
