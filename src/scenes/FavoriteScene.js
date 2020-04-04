95be348af62ef3356b58795b5373eed393dd705d
Sasipreeya Sukprasert <sasipreeya.sukprasert@scb.co.th>
2020-04-04 23:15:34 +0700

    Fixed FavoriteScene code

--- src/scenes/FavoriteScene.js
+++ src/scenes/FavoriteScene.js
@@ -1,7 +1,34 @@
-import React, { Component } from ‘react’
+import React from "react";
+import { ListItem } from "react-native-elements";
+import { connect } from "react-redux";
+import { View } from "react-native";
+import { MovieItem } from "../components/MovieItemComponent";
 
-export default class FavoriteScene extends Component {
-    render() {
-        return ()
-    }
+export class FavoriteScene extends React.Component {
+  render() {
+    const { favoriteList } = this.props;
+    return (
+      <View>
+        {favoriteList.map(result => (
+            {result.map((item, index) => (
+                <MovieItem 
+                    item={item}
+                    title={item.title} 
+                    release_date={item.release_date}
+                    overview={item.overview}
+                    poster_path={item.poster_path}
+                />
+            ))}
+        ))}
+      </View>
+    );
+  }
 }
+
+const mapStateToProps = state => {
+  return {
+    favoriteList: state.favoriteReducer.favoriteList
+  };
+};
+
+export default connect(mapStateToProps)(SearchResultScene);
