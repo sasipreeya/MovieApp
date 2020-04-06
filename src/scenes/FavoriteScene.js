075ec5caa30f0e99c7aab88c98c81fe8804db72f
Sasipreeya Sukprasert <sasipreeya.sukprasert@scb.co.th>
2020-04-06 19:43:47 +0700

    Fixed code in favourite scene follow search result scene

--- src/scenes/FavoriteScene.js
+++ src/scenes/FavoriteScene.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { ListItem } from "react-native-elements";
 import { connect } from "react-redux";
-import { View } from "react-native";
+import { View, ScrollView } from "react-native";
 import { MovieItem } from "../components/MovieItemComponent";
 
 export class FavoriteScene extends React.Component {
   render() {
     const { favoriteList } = this.props;
     return (
-      <View>
-        {favoriteList.map((result) =>
-          result.map((item, index) => (
+      <ScrollView>
+        <View>
+          {favoriteList.map((item, index) => (
             <MovieItem
               item={item}
               title={item.title}
@@ -18,9 +18,9 @@ export class FavoriteScene extends React.Component {
               overview={item.overview}
               poster_path={item.poster_path}
             />
-          ))
-        )}
-      </View>
+          ))}
+        </View>
+      </ScrollView>
     );
   }
 }
