33eeab585b2f9b738faa9fd7c7b67dac5816e38a
Sasipreeya Sukprasert <sasipreeya.sukprasert@scb.co.th>
2020-04-06 19:10:49 +0700

    Shown search result in the right way

--- src/components/MovieItemComponent.js
+++ src/components/MovieItemComponent.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Col from "react-native-col";
-import { View, Image, Text } from "react-native";
+import Row from "react-native-row";
+import { View, Image } from "react-native";
 import { connect } from "react-redux";
+import { Divider, Text } from "react-native-elements";
 
 export const MovieItem = (props) => {
   let { item, title, release_date, overview, poster_path } = props;
@@ -9,34 +11,43 @@ export const MovieItem = (props) => {
   const onItemClicked = (item) => {};
 
   return (
-    <View onStartShouldSetResponder={() => onItemClicked(item)}>
-      <Col.L />
+    <View>
       <View
-        style={{
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "flex-start",
-        }}
+        onStartShouldSetResponder={() => onItemClicked(item)}
+        style={{ flexDirection: "row" }}
       >
-        <Image
-          source={{
-            uri: `https://image.tmdb.org/t/p/w92${poster_path}`,
+        <View
+          style={{
+            padding: 10,
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "flex-start",
           }}
-        />
-      </View>
+        >
+          <Image
+            source={{
+              uri: `https://image.tmdb.org/t/p/w92${poster_path}`,
+            }}
+            style={{ width: 100, height: 140 }}
+          />
+        </View>
 
-      <Col.R />
-      <View
-        style={{
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "flex-end",
-        }}
-      >
-        <Text>{title}</Text>
-        <Text>{release_date}</Text>
-        <Text numberOfLines={4}>{overview}</Text>
+        <View
+          style={{
+            width: 250,
+            padding: 10,
+            flexDirection: "column",
+            alignItems: "flex-start",
+          }}
+        >
+          <Text style={{ fontSize: 20, fontWeight: "bold" }}>{title}</Text>
+          <Text style={{ fontSize: 16, opacity: 0.5 }}>{release_date}</Text>
+          <Text style={{ fontSize: 16 }} numberOfLines={4}>
+            {overview}
+          </Text>
+        </View>
       </View>
+      <Divider style={{ backgroundColor: "gray" }} />
     </View>
   );
 };
